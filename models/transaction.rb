9ccb6c8af2c7bb4d4dbe8aa5a3302595b540f107
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-09 23:34:42 +0100

    adds transaction .update() function, tested in pry and psql.

--- models/transaction.rb
+++ models/transaction.rb
@@ -21,4 +21,11 @@ class Transaction
     SqlRunner.run(sql)
   end
 
+  def update()
+    sql = "UPDATE transactions SET (amount, merchant_id, tag_id) = ($1, $2, $3) WHERE id = $4"
+    values = [@amount, @merchant_id, @tag_id, @id]
+    SqlRunner.run(sql, values)
+
+  end
+
 end
