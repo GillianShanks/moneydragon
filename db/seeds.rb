01c90c302a0cec82cd0dc9a8214858f7c681f95e
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-09 17:31:23 +0100

    adds SqlRunner

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,21 @@ tag2 = Tag.new({
 tag3 = Tag.new({
   "type" => "games"
   })
+
+transaction1 = Transaction.new({
+  "amount" => 15,
+  "merchant_id" => merchant1.id,
+  "tag_id" => tag3.id
+  })
+
+transaction2 = Transaction.new({
+  "amount" => 20,
+  "merchant_id" => merchant3.id,
+  "tag_id" => tag1.id
+  })
+
+transaction3 = Transaction.new({
+  "amount" => 40,
+  "merchant_id" => merchant2.id,
+  "tag_id" => tag2.id
+  })
---
+++ db/sql_runner.rb
@@ -0,0 +1,16 @@
+require("pg")
+
+class SqlRunner
+
+  def self.run(sql, values = [])
+    begin
+      db = PG.connect({dbname: 'moneydragon', host: 'localhost'})
+      db.prepare('query', sql)
+      result = db.exec_prepared('query', values)
+    ensure
+      db.close() if db != nil
+    end
+    return result
+  end
+
+end
