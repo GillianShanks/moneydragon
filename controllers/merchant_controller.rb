a7aa43cd3aff9d7acf5be10d96fe5e98dc521641
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-10 12:45:56 +0100

    adds SHOW merchant page - for updating later with all transactions by merchant

--- controllers/merchant_controller.rb
+++ controllers/merchant_controller.rb
@@ -3,7 +3,7 @@ require_relative('../models/merchant.rb')
 #INDEX
 get '/merchants' do
   @merchants = Merchant.all()
-  erb(:"merchant/index")
+  erb(:"merchants/index")
 end
 
 #NEW
@@ -17,3 +17,8 @@ end
 #DELETE
 
 #SHOW
+get '/merchants/:id' do
+  id=params[:id].to_i()
+  @merchant = Merchant.find(id)
+  erb(:"merchants/show")
+end
