6ee3e5d3558fc937aa4546883dc2cadcfc48149a
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-10 20:31:23 +0100

    adds form to add new transaction and saves it

--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -2,15 +2,23 @@ require_relative('../models/transaction.rb')
 
 #INDEX
 get '/spending' do
-  @transactions = Transaction.all()
+  @spending = Transaction.all()
   erb(:"transactions/index")
 end
 
 #NEW
-
+get '/spending/new' do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  erb(:"transactions/new")
+end
 
 #CREATE
-
+post '/spending' do
+  spending=Transaction.new(params)
+  spending.save()
+  redirect("/spending")
+end
 
 #EDIT
 
